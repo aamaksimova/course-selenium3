a3ef8092624bce52c0fa29318b3b33db75f4c037
Anastasiya Maksimova <naskach12@gmail.com>
2018-03-13 10:23:19 +0400

    Test for all admin pages was update

--- selenium-java/src/test/java/localhost/AllAdminPagesTest.java
+++ selenium-java/src/test/java/localhost/AllAdminPagesTest.java
@@ -1,17 +1,33 @@
 package localhost;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 public class AllAdminPagesTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
+
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
 
     @Before
     public void start() {
@@ -25,8 +41,32 @@ public class AllAdminPagesTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        
+        wait.until(ExpectedConditions.titleIs("My Store"));
 
+        List<WebElement> items = driver.findElements(By.cssSelector("li#app-"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        for (int i=1; i<=items.size(); i++) {
+            items = driver.findElements(By.cssSelector("li#app-"));
+            wait.until(ExpectedConditions.visibilityOf(items.get(i-1)));
+            items.get(i-1).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            Assert.assertTrue(isElementPresent(By.tagName("h1")));
+            System.out.println(i);
+
+            if (isElementPresent(By.cssSelector("ul.docs"))) {
+                List<WebElement> menu = driver.findElements(By.cssSelector("[id ^=doc]"));
+                for (int m=1; m<=menu.size(); m++) {
+                    menu = driver.findElements(By.cssSelector("[id ^=doc]"));
+                    wait.until(ExpectedConditions.visibilityOf(menu.get(m-1)));
+                    menu.get(m-1).click();
+                    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+                    Assert.assertTrue(isElementPresent(By.tagName("h1")));
+                    System.out.println(m);
+                }
+            }
+
+        }
     }
 
     @After
