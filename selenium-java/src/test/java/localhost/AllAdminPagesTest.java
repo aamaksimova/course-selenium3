b7b5aac1406a09a302fceec32931bb7c73c9c9f1
Anastasiya Maksimova <naskach12@gmail.com>
2018-03-13 08:14:19 +0400

    Revert "Merge branch 'master' of https://github.com/aamaksimova/course-selenium3"
    This reverts commit 3986de146eef7b8ad05a5627c90663d619d03db8, reversing
    changes made to 419f672608b778a1089d5d5dba328518a5a65a86.

--- selenium-java/src/test/java/localhost/AllAdminPagesTest.java
+++ selenium-java/src/test/java/localhost/AllAdminPagesTest.java
@@ -25,65 +25,8 @@ public class AllAdminPagesTest {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        driver.findElement(By.linkText("Appearence")).click();
-        driver.findElement(By.id("doc-template")).click();
-        driver.findElement(By.id("doc-logotype")).click();
-        driver.findElement(By.linkText("Catalog")).click();
-        driver.findElement(By.id("doc-catalog")).click();
-        driver.findElement(By.id("doc-product_groups")).click();
-        driver.findElement(By.id("doc-option_groups")).click();
-        driver.findElement(By.id("doc-manufacturers")).click();
-        driver.findElement(By.id("doc-suppliers")).click();
-        driver.findElement(By.id("doc-delivery_statuses")).click();
-        driver.findElement(By.id("doc-sold_out_statuses")).click();
-        driver.findElement(By.id("doc-quantity_units")).click();
-        driver.findElement(By.id("doc-csv")).click();
-        driver.findElement(By.linkText("Countries")).click();
-        driver.findElement(By.linkText("Currencies")).click();
-        driver.findElement(By.linkText("Customers")).click();
-        driver.findElement(By.id("doc-customers")).click();
-        driver.findElement(By.id("doc-csv")).click();
-        driver.findElement(By.id("doc-newsletter")).click();
-        driver.findElement(By.linkText("Geo Zones")).click();
-        driver.findElement(By.linkText("Languages")).click();
-        driver.findElement(By.id("doc-languages")).click();
-        driver.findElement(By.id("doc-storage_encoding")).click();
-        driver.findElement(By.linkText("Modules")).click();
-        driver.findElement(By.id("doc-jobs")).click();
-        driver.findElement(By.id("doc-customer")).click();
-        driver.findElement(By.id("doc-shipping")).click();
-        driver.findElement(By.id("doc-payment")).click();
-        driver.findElement(By.id("doc-order_total")).click();
-        driver.findElement(By.id("doc-order_success")).click();
-        driver.findElement(By.id("doc-order_action")).click();
-        driver.findElement(By.linkText("Orders")).click();
-        driver.findElement(By.id("doc-orders")).click();
-        driver.findElement(By.id("doc-order_statuses")).click();
-        driver.findElement(By.linkText("Pages")).click();
-        driver.findElement(By.linkText("Reports")).click();
-        driver.findElement(By.id("doc-monthly_sales")).click();
-        driver.findElement(By.id("doc-most_sold_products")).click();
-        driver.findElement(By.id("doc-most_shopping_customers")).click();
-        driver.findElement(By.linkText("Settings")).click();
-        driver.findElement(By.id("doc-store_info")).click();
-        driver.findElement(By.id("doc-defaults")).click();
-        driver.findElement(By.id("doc-general")).click();
-        driver.findElement(By.id("doc-listings")).click();
-        driver.findElement(By.id("doc-images")).click();
-        driver.findElement(By.id("doc-checkout")).click();
-        driver.findElement(By.id("doc-advanced")).click();
-        driver.findElement(By.id("doc-security")).click();
-        driver.findElement(By.linkText("Slides")).click();
-        driver.findElement(By.linkText("Tax")).click();
-        driver.findElement(By.id("doc-tax_classes")).click();
-        driver.findElement(By.id("doc-tax_rates")).click();
-        driver.findElement(By.linkText("Translations")).click();
-        driver.findElement(By.id("doc-search")).click();
-        driver.findElement(By.id("doc-scan")).click();
-        driver.findElement(By.id("doc-csv")).click();
-        driver.findElement(By.linkText("Users")).click();
-        driver.findElement(By.linkText("vQmods")).click();
-        driver.findElement(By.id("doc-vqmods")).click();
+        
+
     }
 
     @After
